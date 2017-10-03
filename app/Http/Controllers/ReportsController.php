70c1f1474ce7747d4cc091dec8b76b548e8d1045
Adrian Gutierrez <adrian.gutierrez@vanillasys.com>
2017-10-03 18:42:55 -0500

    replaced name with site code

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -1012,7 +1012,7 @@ class ReportsController extends Controller
 
                 $accountYTDIncComp = AccountSummary::where('accountId', $account->id)->orderBy('MonthEndDate', 'desc')->first();
 
-                $sheetName = $account->name;
+                $sheetName = $account->siteCode;
 
                 $excel->sheet($sheetName, function($sheet) use ($account, $activeRosterPhysicians, $activeRosterAPPs, $benchPhysicians, $benchAPPs, $credentialers, $recruitings, $accountPrevMonthIncComp, $accountYTDIncComp){
                     
