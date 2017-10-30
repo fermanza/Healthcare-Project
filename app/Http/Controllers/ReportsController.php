44816a52f933d65ad6d79d7c465b8eb5a9177e45
Adrian Gutierrez <adrian.gutierrez@vanillasys.com>
2017-10-29 21:59:34 -0600

    changed sheet title rules

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -1321,6 +1321,7 @@ class ReportsController extends Controller
 
                 $sheetName = (strlen($account->name) > 31) ? substr($account->name,0,28).'...' : $account->name;
                 $sheetName = str_replace("/", "_", $sheetName);
+                $sheetName = str_replace("?", "_", $sheetName);
 
                 $excel->sheet($sheetName, function($sheet) use ($account, $percentRecruitedPhys, $percentRecruitedApp, $percentRecruitedPhysReport, $percentRecruitedAppReport, $activeRosterPhysicians, $activeRosterAPPs, $benchPhysicians, $benchAPPs, $recruitings, $locums, $declines, $resigneds, $credentialersPhys, $credentialersAPP, $SMDOpen, $AMDOpen, $tableStyle, $headerStyle, $topBorder, $bottomBorder){
 
