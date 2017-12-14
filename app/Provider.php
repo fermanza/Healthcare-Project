e8d7eb07065f910c696fcf68247edf49362c0837
Adrian Gutierrez <adrian.gutierrez@vanillasys.com>
2017-12-13 20:58:02 -0700

    utf8 decode for provider names

--- app/Provider.php
+++ app/Provider.php
@@ -10,4 +10,37 @@ class Provider extends Model
      * @var string
      */
     protected $table = 'tProvider';
+
+    /**
+     * Get the fullName.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getFullNameAttribute($value)
+    {
+        return utf8_decode($value);
+    }
+
+    /**
+     * Get the firstName.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getFirstNameAttribute($value)
+    {
+        return utf8_decode($value);
+    }
+
+    /**
+     * Get the lastName.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getLastNameAttribute($value)
+    {
+        return utf8_decode($value);
+    }
 }
